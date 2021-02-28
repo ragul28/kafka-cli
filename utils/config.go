9441e96da4ec63cae56a4575f9c1355ecc606d56
ragul28 <ragulh28@gmail.com>
2021-02-28 22:00:04 +0530

    Add utils pkg for config

---
+++ utils/config.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Confg struct {
+	Broker     string
+	Protocol   string
+	Mechanisms string
+	Username   string
+	Password   string
+	Groupid    string
+	Topic      string
+}
+
+func LoadConfig() *Confg {
+	var c Confg
+
+	c.Broker = getConfig("KAFKA_BROKERS", "broker", "localhost:9200", "Kafka broker url")
+	c.Protocol = getConfig("KAFKA_PROTOCOL", "protocol", "PLAINTEXT", "Kafka broker protocol")
+	c.Mechanisms = getConfig("KAFKA_SASLMEC", "SASL", "PLAIN", "Kafka SASL")
+	c.Username = getConfig("KAFKA_USERNAME", "user", "", "Kafka broker user")
+	c.Password = getConfig("KAFKA_PASSWORD", "password", "", "Kafka broker password")
+	c.Groupid = getConfig("KAFKA_GROUPID", "gid", "gid", "Kafka group id")
+	c.Topic = getConfig("KAFKA_TOPIC", "topic", "topic", "Kafka topic name")
+
+	return &c
+}
+
+func getConfig(key, flagStr, defaultVal, flagUsage string) string {
+
+	flagValue := flag.String(flagStr, "", flagUsage)
+
+	fmt.Println(*flagValue, len(*flagValue))
+
+	if len(*flagValue) == 0 {
+		sysEnv := os.Getenv(key)
+		if len(sysEnv) == 0 {
+			return defaultVal
+		}
+		fmt.Println(sysEnv)
+		return sysEnv
+	}
+
+	return *flagValue
+}
